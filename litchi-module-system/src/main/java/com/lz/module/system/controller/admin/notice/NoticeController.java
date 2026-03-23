fedb29b5d9f664e44e45e6ae6881b152f9929b7e
SpringSunYY <3217778348@qq.com>
2026-03-23 19:59:38 +0800

    通知公告-添加附件，推送功能

--- litchi-module-system/src/main/java/com/lz/module/system/controller/admin/notice/NoticeController.java
+++ litchi-module-system/src/main/java/com/lz/module/system/controller/admin/notice/NoticeController.java
@@ -1,16 +1,20 @@
 package com.lz.module.system.controller.admin.notice;
 
 import cn.hutool.core.lang.Assert;
+import com.lz.framework.common.biz.system.permission.PermissionCommonApi;
+import com.lz.framework.common.enums.CommonStatusEnum;
 import com.lz.framework.common.enums.UserTypeEnum;
 import com.lz.framework.common.pojo.CommonResult;
 import com.lz.framework.common.pojo.PageResult;
 import com.lz.framework.common.util.object.BeanUtils;
+import com.lz.framework.security.core.util.SecurityFrameworkUtils;
 import com.lz.module.infra.api.websocket.WebSocketSenderApi;
 import com.lz.module.system.controller.admin.notice.vo.NoticePageReqVO;
 import com.lz.module.system.controller.admin.notice.vo.NoticeRespVO;
 import com.lz.module.system.controller.admin.notice.vo.NoticeSaveReqVO;
 import com.lz.module.system.dal.dataobject.notice.NoticeDO;
 import com.lz.module.system.service.notice.NoticeService;
+import com.lz.module.system.service.notify.NotifySendService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -22,7 +26,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static com.lz.framework.common.exception.util.ServiceExceptionUtil.exception;
 import static com.lz.framework.common.pojo.CommonResult.success;
+import static com.lz.module.system.enums.ErrorCodeConstants.NOTICE_DISABLE;
 
 @Tag(name = "管理后台 - 通知公告")
 @RestController
@@ -34,7 +40,13 @@ public class NoticeController {
     private NoticeService noticeService;
 
     @Resource
+    private NotifySendService notifySendService;
+
+    @Resource
     private WebSocketSenderApi webSocketSenderApi;
+
+    @Resource
+    private PermissionCommonApi permissionApi;
 
     @PostMapping("/create")
     @Operation(summary = "创建通知公告")
@@ -74,6 +86,13 @@ public class NoticeController {
     @Operation(summary = "获取通知公告列表")
     @PreAuthorize("@ss.hasPermission('system:notice:query')")
     public CommonResult<PageResult<NoticeRespVO>> getNoticePage(@Validated NoticePageReqVO pageReqVO) {
+        Long userId = SecurityFrameworkUtils.getLoginUserId();
+
+        // 检查用户是否有指定权限
+        boolean hasPermission = permissionApi.hasAnyPermissions(userId, "system:notice:create");
+        if (!hasPermission) {
+            pageReqVO.setStatus(CommonStatusEnum.ENABLE.getStatus());
+        }
         PageResult<NoticeDO> pageResult = noticeService.getNoticePage(pageReqVO);
         return success(BeanUtils.toBean(pageResult, NoticeRespVO.class));
     }
@@ -94,6 +113,13 @@ public class NoticeController {
     public CommonResult<Boolean> push(@RequestParam("id") Long id) {
         NoticeDO notice = noticeService.getNotice(id);
         Assert.notNull(notice, "公告不能为空");
+        //如果公告状态不是已发布
+        boolean equals = !notice.getStatus().equals(CommonStatusEnum.ENABLE.getStatus());
+        if (equals) {
+            throw exception(NOTICE_DISABLE);
+        }
+        //发送站内信
+        notifySendService.sendNoticeToAdmin(notice);
         // 通过 websocket 推送给在线的用户
         webSocketSenderApi.sendObject(UserTypeEnum.ADMIN.getValue(), "notice-push", notice);
         return success(true);
