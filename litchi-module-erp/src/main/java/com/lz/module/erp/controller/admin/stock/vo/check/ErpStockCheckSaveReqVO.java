8a6a8a552612405828df3b1beaba15f6df45fd10
SpringSunYY <3217778348@qq.com>
2026-05-29 20:39:37 +0800

    erp：国际化异常和字段

--- litchi-module-erp/src/main/java/com/lz/module/erp/controller/admin/stock/vo/check/ErpStockCheckSaveReqVO.java
+++ litchi-module-erp/src/main/java/com/lz/module/erp/controller/admin/stock/vo/check/ErpStockCheckSaveReqVO.java
@@ -4,8 +4,6 @@ import com.lz.framework.common.validation.i18n.I18nNotEmpty;
 import com.lz.framework.common.validation.i18n.I18nNotNull;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
